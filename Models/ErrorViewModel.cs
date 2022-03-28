cd97542800aa20e6e989fb62c0ea49f01c8431aa
XuanSon <112113son@gmail.com>
2022-03-28 07:24:20 +0700

    First_commit_NXS537_2022

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace NguyenXuanSon2022537.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
